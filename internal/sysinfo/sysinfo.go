805847a917ab89dbd81ee6c2d8c89e11e77cb15c
Bocaletto Luca <58043613+bocaletto-luca@users.noreply.github.com>
2025-06-13 08:42:38 +0200

    Create sysinfo.go

---
+++ internal/sysinfo/sysinfo.go
@@ -0,0 +1,63 @@
+// File: internal/sysinfo/sysinfo.go
+package sysinfo
+
+import (
+    "fmt"
+    "strings"
+
+    "github.com/shirou/gopsutil/v3/cpu"
+    "github.com/shirou/gopsutil/v3/host"
+    "github.com/shirou/gopsutil/v3/mem"
+    "github.com/shirou/gopsutil/v3/net"
+    "github.com/shirou/gopsutil/v3/process"
+)
+
+// Data holds collected system metrics.
+type Data struct {
+    IPs        []string
+    Hostname   string
+    OS         string
+    CPUPercent []float64
+    MemTotal   uint64
+    MemUsed    uint64
+    Users      []string
+}
+
+// Collect gathers requested modules' data.
+func Collect(modules []string) *Data {
+    d := &Data{}
+    for _, m := range modules {
+        switch strings.ToLower(m) {
+        case "ip":
+            ifs, _ := net.Interfaces()
+            for _, iface := range ifs {
+                for _, addr := range iface.Addrs {
+                    d.IPs = append(d.IPs, addr.Addr)
+                }
+            }
+        case "os":
+            hi, _ := host.Info()
+            d.Hostname = hi.Hostname
+            d.OS = fmt.Sprintf("%s %s", hi.Platform, hi.PlatformVersion)
+        case "cpu":
+            pct, _ := cpu.Percent(0, false)
+            d.CPUPercent = pct
+        case "mem":
+            mi, _ := mem.VirtualMemory()
+            d.MemTotal = mi.Total
+            d.MemUsed = mi.Used
+        case "users":
+            procs, _ := process.Processes()
+            seen := map[string]struct{}{}
+            for _, p := range procs {
+                if u, err := p.Username(); err == nil {
+                    if _, ok := seen[u]; !ok {
+                        d.Users = append(d.Users, u)
+                        seen[u] = struct{}{}
+                    }
+                }
+            }
+        }
+    }
+    return d
+}
